d320bf6d7121993b53c5806b2c759fb3dbe63585
kalen510 <kalen.wiley@gmail.com>
2023-05-04 16:08:27 -0400

    preliminary delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,26 @@
 const router = require('express').Router();
 const notes = require('../../db/db');
-const { createNewNote } = require('../../lib/notes');
+const { createNewNote, deleteNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
-    let results = notes;
+    const results = notes;
     res.json(results);
 });
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
-    let updatedNotes = createNewNote(req.body);
-    return res.json(updatedNotes);
+    const newNote = createNewNote(req.body, notes);
+    return res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const updatedNotes = deleteNote(req.params.id, notes);
+    if (updatedNotes) {
+        console.log("response sent")
+        return res.json(updatedNotes);
+    } else {
+        res.send(404);
+    }
 });
 
 module.exports = router;
