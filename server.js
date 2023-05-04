df12932932ead36779075db5e1824bffa5fd52d8
kalen510 <kalen.wiley@gmail.com>
2023-05-04 14:09:08 -0400

    set up express server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// 2. set up port usage for deployment environment
+const PORT = process.env.PORT || 3001;
+
+// 1. instatiate server
+const app = express();
+
+// 4. apply middleware
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: treu }));
+// parse incoming JSON data
+app.use(express.json());
+// make public folder readily available
+app.use(express.static('public'));
+// middleware for router - modularized api calls
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// 3. listen for requests to server
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
